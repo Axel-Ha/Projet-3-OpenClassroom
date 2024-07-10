c5ed5cc4bacd52c9e7d93afda2505af0efc1241a
Axel Halifa <axel.halifa0@gmail.com>
2024-07-10 09:48:05 +0200

    Modification des dependances a permis d'acceder à swagger

--- Back-End/SpringSecurityAuth/src/main/java/com/chatop/api/SpringSecurityAuthApplication.java
+++ Back-End/SpringSecurityAuth/src/main/java/com/chatop/api/SpringSecurityAuthApplication.java
@@ -2,8 +2,8 @@ package com.chatop.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
